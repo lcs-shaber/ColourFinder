94d5a6dfbf4300b4e301f859201eb08d30a6022f
Sophie Haber <SHaber@lcs.on.ca>
2025-04-03 12:18:46 -0400

    Added a slider to allow users to change hue of colour swatch.

--- ColourFinder/Views/CreateColoureView.swift
+++ ColourFinder/Views/CreateColoureView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct CreateColoureView: View {
+    
+    //MARK: Stored properties
+    @State var currenHue = 0.0
+    
+    //MARK: Computed properties
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Slider(value: $currenHue, in: 0...360.0)
+            Rectangle()
+                .fill(
+                    Color(
+                        hue: currenHue/360.0,
+                        saturation: 100.0/100.0,
+                        brightness: 100.0/100.0
+                    )
+                )
+                .frame(width: 100, height: 100)
+                
         }
         .padding()
     }
