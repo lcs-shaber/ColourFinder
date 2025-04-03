2ff899d037e99eb48840cd7e93ac989ecd00578e
Sophie Haber <SHaber@lcs.on.ca>
2025-04-03 12:06:07 -0400

    Initial Commit

---
+++ ColourFinder/ColourFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourFinderApp.swift
+//  ColourFinder
+//
+//  Created by Sophie Haber on 03.04.25.
+//
+
+import SwiftUI
+
+@main
+struct ColourFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
